4a36db866c3da1338bcd634f3bd0b900e31a823f
Rohit Mahto <rohitmahto267@gmaill.com>
2021-08-14 20:22:51 +0530

    initial front-end setup

---
+++ app.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT;
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
