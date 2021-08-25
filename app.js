55e957690309edfff9473ce26d9a18803faf6174
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2021-08-26 06:32:09 +0900

    feat: display the icon while logged in

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const authRoute = require('./routes/Auth');
 
 //static serve
 app.use(express.static(path.join(__dirname, "build")));
-app.use("/avatar", express.static('avatars'));
+app.use("/avatars", express.static('avatars'));
 //Auth route
 app.use('/auth', authRoute);
 
--- front-end/src/components/Navbar/index.js
+++ front-end/src/components/Navbar/index.js
@@ -1,21 +1,28 @@
 import React, { useState } from 'react';
-import { Flex, Heading, IconButton, Spacer, Button } from '@chakra-ui/react';
+import { Flex, Heading, IconButton, Spacer, Button, Image } from '@chakra-ui/react';
 import { FaSearch, FaUser } from 'react-icons/fa';
 import SearchBox from '../SearchBox';
 import { useHistory } from 'react-router';
+import { useSelector } from 'react-redux'
 
 function Index(props) {
     const [searchVisible, tootleSearch] = useState(false);
     const history = useHistory();
+    const user = useSelector(state => state.auth);
 
     return (
         <Flex p='16px' boxShadow="0px 1px 8px #b9b9b9">
             {/* <Container> */}
             <Heading ml="8" fontSize='24px' fontWeight='semibold' size='md' color='cyan.600' >AkSepya</Heading>
             <Spacer />
-            <Button onClick={(e) => history.push("/login")} ml='2' pl='8px' pr='8px' leftIcon={<FaUser />} variant="solid">
-                Log In
+            { user.isLoggedIn ? (
+                <Image src={user.avatar} boxSize="40px" borderRadius="full"/>
+            ): (
+                <Button onClick={(e) => history.push("/login")} ml='2' pl='8px' pr='8px' leftIcon={<FaUser />} variant="solid">
+                  Log In
                 </Button>
+            )}
+            
             <IconButton ml='2' icon={<FaSearch />} onClick={() => tootleSearch(!searchVisible)} />
             <SearchBox visible={searchVisible} />
             {/* </Container> */}
